ae8094f12c69bda25b48c2166d4c82b1b40fa72c
SANJAY-P-M <pmsanjay2003@gmail.com>
2023-09-16 22:22:27 +0530

    Improved code Hygiene

---
+++ Queue.js
@@ -0,0 +1,19 @@
+class Queue{
+    constructor(){
+        this.array = [];
+    }
+    enqueue(element){
+        this.array.push(element);
+    }
+    dequeue(){
+        return this.array.shift();
+    }
+    // To create Elements with items in queue we use this method ,
+    // Making this method separate will ensure the entire queue is not accessible but only the elements
+
+    forEachElements(doThis){
+        this.array.forEach(doThis);
+    }
+}
+
+export default Queue;
--- hand_cericket.js
+++ hand_cericket.js
@@ -1,12 +1,20 @@
+import Queue from './Queue'
+
+var queue = new Queue();
+
+// This method will be called when we click "---THROW NEXT---" button
 function changeNext(){
-    var image =  document.getElementById("image");
-    var imagePath = image.src;
-    var previousNumber = imagePath[imagePath.length - 5];
+    // Jammed in building this part 
+    let image =  document.getElementById("image");
+
+    let previousNumber = image.src[imagePath.length - 5];
+    queue.enqueue(previousNumber);
     let number  = Math.floor(Math.random() * 5) + 1;
     image.src = 'images/hand-cricket'+number+'.png';
-    var scoreRecoder = document.getElementById('scoreRecorder');
-    console.log(scoreRecoder.childNodes);
+    queue.forEachElements((element) => document.getElementById("scoreRecorder").appendChild(document.createElement(div)));
 }
+
+// This method will be called when we click "---RESET---" button
 function reset(){
     document.getElementById('image').src = 'images/hand-cricket0.png';
 }
