2c7e1cbaaa458d2ec81ffa16d5f4d8005235a27c
SANJAY-P-M <pmsanjay2003@gmail.com>
2023-09-18 22:05:08 +0530

    Added total score keeper

--- hand_cricket.js
+++ hand_cricket.js
@@ -1,15 +1,21 @@
 import Queue from './Queue.js'
 
-var queueForCricket = new Queue();
+const queueForCricket = new Queue();
 
 // This method will be called when we click "---THROW NEXT---" button
 window.changeNext = function changeNext(){
     // Since this function is in module scope when I called in onclick it shows an error 
     // To change this to global scope explicitly I used window 
-    let image =  document.getElementById("image");
+    const image =  document.getElementById("image");
 
     // Got Score with file name
-    let previousScore = image.src[image.src.length - 5];
+    const previousScore = image.src[image.src.length - 5];
+    
+    // Updating total score
+    const sumElement = document.getElementById('sum');
+    const currentScore = parseInt(sumElement.textContent);
+    sumElement.textContent =currentScore +  parseInt(previousScore);
+
     console.log(queueForCricket.queueLength());
     if(queueForCricket.queueLength() == 6){
         queueForCricket.dequeue();
@@ -19,16 +25,14 @@ window.changeNext = function changeNext(){
     }
 
     // updating image with random numbers
-    let number  = Math.floor(Math.random() * 5) + 1;
+    const number  = Math.floor(Math.random() * 5) + 1;
     image.src = 'images/hand-cricket'+number+'.png';
 
     // updating scoreRecorder
-    let scoreRecorder = document.getElementById('scoreRecorder');
-    while (scoreRecorder.firstChild) {
-        scoreRecorder.removeChild(scoreRecorder.firstChild);
-    }
+    const scoreRecorder = document.getElementById('scoreRecorder');
+    scoreRecorder.textContent = '';
     queueForCricket.forEachElements((element) => {
-        let scoreCard = document.createElement('div');
+        const scoreCard = document.createElement('div');
         scoreCard.className = 'score';
         scoreCard.innerText = element;
         scoreRecorder.appendChild(scoreCard);
@@ -37,8 +41,6 @@ window.changeNext = function changeNext(){
 
 // This method will be called when we click "---RESET---" button
 window.reset = function reset(){
-    while (scoreRecorder.firstChild) {
-        scoreRecorder.removeChild(scoreRecorder.firstChild);
-    }
+    scoreRecorder.textContent = '';
     document.getElementById('image').src = 'images/hand-cricket0.png';
 }
